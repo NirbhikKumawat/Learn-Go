1fc7cfedbaede24a196d8d95bb392d75193b6b74
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2025-10-09 08:51:45 +0530

    enums.go

---
+++ enums.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type ServerState int
+
+const (
+	StateIdle ServerState = iota
+	StateConnected
+	StateError
+	StateRetrying
+)
+
+var stateName = map[ServerState]string{
+	StateIdle:      "Idle",
+	StateConnected: "Connected",
+	StateError:     "Error",
+	StateRetrying:  "Retrying",
+}
+
+func (ss ServerState) String() string {
+	return stateName[ss]
+}
+func main() {
+	ns := transition(StateIdle)
+	fmt.Println(ns)
+	ns2 := transition(ns)
+	fmt.Println(ns2)
+}
+func transition(state ServerState) ServerState {
+	switch state {
+	case StateIdle:
+		return StateConnected
+	case StateConnected, StateRetrying:
+		return StateIdle
+	case StateError:
+		return StateError
+	default:
+		panic("Unknown server state")
+	}
+}
