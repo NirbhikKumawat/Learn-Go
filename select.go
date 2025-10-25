d5581770144aafc069e7cc9f42fef9a99c71bf50
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2025-10-25 13:48:04 +0530

    select.go

---
+++ select.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+	go func() {
+		time.Sleep(time.Second * 1)
+		c1 <- "ping1"
+	}()
+	go func() {
+		time.Sleep(time.Second * 2)
+		c2 <- "ping2"
+	}()
+	for range 2 {
+		select {
+		case msg := <-c1:
+			fmt.Println(msg)
+		case msg := <-c2:
+			fmt.Println(msg)
+		}
+	}
+}
